2bc1daa134ee54d277a1b269271513927df69998
Toish <toish@toi.sh>
2020-11-22 00:49:43 +0000

    added background

--- src/layout/Shell.tsx
+++ src/layout/Shell.tsx
@@ -3,9 +3,16 @@ import 'normalize.css'
 import '../styles/variables.css'
 import '../styles/grid.css'
 import styled from 'styled-components'
+
+import imgBackground from '../assets/images/background.svg'
+
 import { Sidebar } from './Sidebar'
 
 const Container = styled.div`
+  background: url(${imgBackground}) no-repeat right -25vw top 10vh / 100vw;
+`
+
+const Content = styled.div`
   min-height: 100vh;
   padding: 40px 20px;
   
@@ -16,11 +23,13 @@ const Container = styled.div`
 
 export function Shell (props: React.PropsWithChildren<{}>) {
   return (
-    <Container className="grid">
-      <Sidebar />
-      <main>
-        {props.children}
-      </main>
+    <Container>
+      <Content className="grid">
+        <Sidebar />
+        <main>
+          {props.children}
+        </main>
+      </Content>
     </Container>
   )
 }
