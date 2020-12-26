02d314ed3214012d0fbf9c452e504e42526c1a8e
Toish <toish@toi.sh>
2020-12-26 17:23:13 +0000

    Updated projects

--- src/mdx.tsx
+++ src/mdx.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import { MDXProvider } from "@mdx-js/react"
 import { Link } from 'gatsby'
+import { OutboundLink } from 'gatsby-plugin-google-gtag'
 import { Emphasis } from './components/fun/emphasis'
 import { Annotated } from './components/fun/annotated'
 
-export const shortcodes = { Link, Emphasis, Annotated }
+export const shortcodes = { Link, OutboundLink, Emphasis, Annotated }
 
 export const MDXProviderWithShortcodes: React.FC = ({children}) => (
   <MDXProvider components={shortcodes}>
