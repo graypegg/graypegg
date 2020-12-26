ef4117ba0201d8f40ec46c927bc4cb981f5e982d
Toish <toish@toi.sh>
2020-12-26 19:01:04 +0000

    Added fun rainbow emphasis tag

---
+++ src/components/fun/rainbow.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.em`
+  font-style: normal;
+  display: inline-block;
+  animation: 4s linear infinite running rainbow;
+  color: transparent;
+  background: var(--gradient-top-left-reversing);
+  background-size: 200% 100%;
+  background-clip: text;
+  -webkit-background-clip: text; // Safari support.
+  
+  @keyframes rainbow {
+    0%{background-position:0% 50%}
+    100%{background-position:200% 50%}
+  }
+`
+
+export function Rainbow (props: React.PropsWithChildren<{}>) {
+  return <Container>
+    {props.children}
+  </Container>;
+}
--- src/mdx.tsx
+++ src/mdx.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react'
-import { MDXProvider } from "@mdx-js/react"
+import { MDXProvider } from '@mdx-js/react'
 import { Link } from 'gatsby'
 import { OutboundLink } from 'gatsby-plugin-google-gtag'
 import { Emphasis } from './components/fun/emphasis'
 import { Annotated } from './components/fun/annotated'
+import { Rainbow } from './components/fun/rainbow'
 
-export const shortcodes = { Link, OutboundLink, Emphasis, Annotated }
+export const shortcodes = {
+  Link,
+  OutboundLink,
+  Emphasis,
+  Annotated,
+  Rainbow
+}
 
 export const MDXProviderWithShortcodes: React.FC = ({children}) => (
   <MDXProvider components={shortcodes}>
